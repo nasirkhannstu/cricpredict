f501dbd2ea9ed9b7f308a706a9bec8e06b075a6f
Nasir Khan <1nasir2khan3@gmail.com>
2017-01-29 19:10:45 +0600

    MultiAuth

---
+++ app/Http/Middleware/DeniedIfNotAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class DeniedIfNotAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = 'admin_user')
+    {
+        if( ! \Auth::guard($guard)->check()){
+            // return response("Access Denied", 403);
+            abort(403);
+        }
+        return $next($request);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('admin_login', 'AdminAuth\LoginController@showLoginForm');
+Route::post('admin_login', 'AdminAuth\LoginController@login');
+Route::post('admin_logout', 'AdminAuth\LoginController@logout');
+Route::post('admin_password/email', 'AdminAuth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('admin_password/reset', 'AdminAuth\ForgotPasswordController@showLinkRequestForm');
+Route::post('admin_password/reset', 'AdminAuth\ResetPasswordController@reset');
+Route::get('admin_password/reset/{token}', 'AdminAuth\ResetPasswordController@showResetForm');
+// Route::get('admin_register', 'AdminAuth\RegisterController@showRegistrationForm');
+// Route::post('admin_register', 'AdminAuth\RegisterController@register');
+
+Route::get('/home', 'HomeController@index');
+Route::get('/admin_home', 'AdminHomeController@index');
